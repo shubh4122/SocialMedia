ec9b0c0ccdd10f3fac81ef40fc7bfc73c6503249
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-04-25 01:04:22 +0530

    Database Class to add data to DB

--- app/src/main/java/com/android/powerlifting/firebase/Database.java
+++ app/src/main/java/com/android/powerlifting/firebase/Database.java
@@ -1,5 +1,28 @@
 package com.android.powerlifting.firebase;
 
-public class Database {
+import com.android.powerlifting.models.Member;
+import com.android.powerlifting.models.Post;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+public class Database {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference postsDB;
+    private DatabaseReference membersDB;
+
+    public Database() {
+        firebaseDatabase = FirebaseDatabase.getInstance();
+    }
+
+    //See is it okay to initialize membersDB and postsDB here.
+    //Or must be done in CONSTRUCTOR?
+    public void addMembers(Member member) {
+        membersDB = firebaseDatabase.getReference().child("Members");
+        membersDB.push().setValue(member);
+    }
+
+    public void addPosts(Post post) {
+        postsDB = firebaseDatabase.getReference().child("Posts");
+        postsDB.push().setValue(post);
+    }
 }
