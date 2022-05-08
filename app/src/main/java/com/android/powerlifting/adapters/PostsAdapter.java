b914efa34ea7d204c94c474b37bf18e1755d1362
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-05-08 11:50:01 +0530

    Added Post layouts

--- app/src/main/java/com/android/powerlifting/adapters/PostsAdapter.java
+++ app/src/main/java/com/android/powerlifting/adapters/PostsAdapter.java
@@ -1,34 +1,72 @@
 package com.android.powerlifting.adapters;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.powerlifting.R;
+import com.android.powerlifting.models.Post;
+
+import java.util.ArrayList;
+
 public class PostsAdapter extends RecyclerView.Adapter<PostsAdapter.PostsViewHolder> {
 
+    private ArrayList<Post> postsList;
+    public PostsAdapter(ArrayList<Post> postsList) {
+        this.postsList = postsList;
+    }
 
     @NonNull
     @Override
     public PostsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.post_item, parent, false);
+        return new PostsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull PostsViewHolder holder, int position) {
+        Post currentPost = postsList.get(position);
 
+        //Get these pics from GLIDE, and db
+//        holder.profilePic.setImageResource(R.drawable.ic_launcher_background);
+//        holder.postPic.setImageResource(R.drawable.ic_launcher_background);
+
+        holder.adminName.setText("ADMIN NAME");
+        holder.time_place.setText("09:12 am | Kanpur, UP");
+//        holder.caption.setText("GET TEXT AND PASTE!!");
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+//        int itemCount = -1;
+//        try {
+//            itemCount = postsList.size();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//        return itemCount;
+        return postsList.size();
     }
 
-    class PostsViewHolder extends RecyclerView.ViewHolder {
+    public class PostsViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView profilePic, postPic;
+        TextView adminName, time_place, caption;
 
         public PostsViewHolder(@NonNull View itemView) {
             super(itemView);
+
+            profilePic = itemView.findViewById(R.id.profilePic);
+            postPic = itemView.findViewById(R.id.post_pic);
+            adminName = itemView.findViewById(R.id.name_of_post_creator);
+            time_place = itemView.findViewById(R.id.time_place_of_post);
+            caption = itemView.findViewById(R.id.caption_post);
+
         }
     }
 }
--- app/src/main/java/com/android/powerlifting/ui/fragment/PostFragment.java
+++ app/src/main/java/com/android/powerlifting/ui/fragment/PostFragment.java
@@ -8,15 +8,66 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.powerlifting.R;
+import com.android.powerlifting.adapters.PostsAdapter;
+import com.android.powerlifting.models.Member;
+import com.android.powerlifting.models.Post;
+
+import java.util.ArrayList;
 
 public class PostFragment extends Fragment {
+
+    private ArrayList<Post> postsList;
+    private RecyclerView recyclerView;
+    private PostsAdapter postsAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_post, container, false);
+        postsList = new ArrayList<Post>();
+
+        String caption = "to create a single activity with multiple fragments in the application or you can create multiple activities as well.\n" +
+                "\n" +
+                "But the major problem with the fragment transaction was its complex structured application. Android Jetpack Navigation controller provides you with several classes to deal with the fragment transaction\n" +
+                "\n" +
+                "Hassle-free fragment transaction by using Navigation Artitecture component\n" +
+                "\n" +
+                "Principle of Navigation\n" +
+                "Fixed Start Destination\n" +
+                "Navigation state is represented as a stack of destinations\n" +
+                "Up and Back are identical within your app’s task\n" +
+                "The Up button never exits your app\n" +
+                "Deep linking simulates manual navigation\n" +
+                "Advantage of using Navigation Controller\n" +
+                "Automatic handling of fragment transactions\n" +
+                "Correctly handling up and back by default\n" +
+                "Default behaviors for animations and transitions\n" +
+                "Deep linking as a first-class operation\n" +
+                "Implementing navigation UI patterns (like navigation drawers and bottom navigation) with a small amount of code\n" +
+                "Type safety arguments when passing information while navigating";
+
+        Member user = new Member("hello", "e43234234234", 213.12f, "sfwefsdf", 12, "Lcochdf");
+        postsList.add(new Post(caption, "sdfsdfsdf", user));
+        postsList.add(new Post(caption, "sdfsdfsdf", user));
+        postsList.add(new Post(caption, "sdfsdfsdf", user));
+        postsList.add(new Post(caption, "sdfsdfsdf", user));
+        postsList.add(new Post(caption, "sdfsdfsdf", user));
+        View view = inflater.inflate(R.layout.fragment_post, container, false);
+        setUpRecyclerView(view);
+        return view;
+    }
+
+    private void setUpRecyclerView(View view) {
+        recyclerView = view.findViewById(R.id.postRecyclerView);
+        layoutManager = new LinearLayoutManager(getActivity());
+        postsAdapter = new PostsAdapter(postsList);
+
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(postsAdapter);
     }
 }
