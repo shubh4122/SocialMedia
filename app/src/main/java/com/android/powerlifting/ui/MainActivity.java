84357a9387a5a6fc15f14d032973d06236c25643
mishra_abhi <abhi200108010@gmail.com>
2022-05-18 23:08:54 +0530

    added logout option

--- app/src/main/java/com/android/powerlifting/ui/MainActivity.java
+++ app/src/main/java/com/android/powerlifting/ui/MainActivity.java
@@ -5,17 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.android.powerlifting.R;
+import com.android.powerlifting.ui.auth.Login;
 import com.android.powerlifting.ui.fragment.AdminFragment;
 import com.android.powerlifting.ui.fragment.MemberFragment;
 import com.android.powerlifting.ui.fragment.PostFragment;
 import com.android.powerlifting.ui.fragment.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
 
     private BottomNavigationView bottomNav;
     private Fragment selectedFragment;   // General Var for selected Fragment
@@ -80,4 +87,47 @@ public class MainActivity extends AppCompatActivity {
                     return true;
                 }
             };
+
+
+    //========================================================================================
+    // Below code is related to phone authentication part
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        checkUserStatus();
+    }
+
+    private void checkUserStatus() {
+        SharedPreferences sharedPreferences=getSharedPreferences("logindata",MODE_PRIVATE);
+        Boolean counter=sharedPreferences.getBoolean("logincounter",Boolean.valueOf(String.valueOf(MODE_PRIVATE)));
+        if(!counter){
+            startActivity(new Intent(MainActivity.this, Login.class));
+            finish();
+        }
+    }
+
+    // code for Logout option
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.logout_menu , menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.logout) {
+            mAuth=FirebaseAuth.getInstance();
+            mAuth.signOut();
+
+            SharedPreferences sharedPreferences=getSharedPreferences("logindata",MODE_PRIVATE);
+            sharedPreferences.edit().clear().commit();
+
+            Intent intent = new Intent(MainActivity.this, Login.class);
+            startActivity(intent);
+            finish();
+        }
+        return true;
+    }
 }
