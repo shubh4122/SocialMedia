467aff8a1aa57f7bb8748f06eb35ae78e0d0d273
Shubham Pandey <shubhampandeylearning@gmail.com>
2022-05-25 14:12:31 +0530

    Made all images centercrop

---
+++ app/src/main/java/com/android/powerlifting/ui/ImageDisplayActivity.java
@@ -0,0 +1,16 @@
+package com.android.powerlifting.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.powerlifting.R;
+
+public class ImageDisplayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_display);
+    }
+}
